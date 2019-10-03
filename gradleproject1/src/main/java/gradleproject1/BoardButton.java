5ec1116545d7be148e8fd93367bcb7cacccbebd3
Henry rheault <henry.rheault@gmail.,com>
2019-10-03 16:59:17 -0500

    Added features to draw(), added methods to initialize the board to new game status, set static on applicable fields, added "NULL" as default/missing piece field as piece ID zero.

--- gradleproject1/src/main/java/gradleproject1/BoardButton.java
+++ gradleproject1/src/main/java/gradleproject1/BoardButton.java
@@ -1,5 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+ * To change this lic
+    
+    //enum for piece values
+    private static final int PAWN = 1;
+    private static final int KNIGHT = 2;
+    private static final int BISHOP = 3;
+    private static final int ense header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -15,39 +21,44 @@ public class BoardButton {
     int row;
     int column;
     
-    //enum for piece values
-    private static final int PAWN = 1;
-    private static final int KNIGHT = 2;
-    private static final int BISHOP = 3;
-    private static final int ROOK = 4;
+    private static final int PAWN=1;
+    private static final int KNIGHT=2;
+    private static final int BISHOP=3;
+    private static final int ROOK=4;
     private static final int QUEEN = 5;
     private static final int KING = 6;
+    private static final int NULL=0;
     //specific piece value stored here
-    private int pieceNumber;
+    private static int pieceNumber;
     
-    boolean isClicked;      //is specific square clicked
-    boolean isWhite;        //is specific square occupied by white piece
-    private static boolean somethingClicked;    //is anything on the board clicked
+    static boolean isClicked;      //is specific square clicked
+    static boolean isWhite;        //is specific square white in color, else false
+    static boolean isPieceWhite;    //Is piece on specific square white, else false
+    private boolean somethingClicked;    //is anything on the board clicked, can't be static
     
     //Gameboard
-    private BoardButton[][] GameBoard = new BoardButton[rows][columns];
+    static private BoardButton[][] GameBoard = new BoardButton[rows][columns];
     
-    public int getPiece(){
-        return this.pieceNumber;
+    static private char c;
+    static private short rowOffset;
+    
+    public static int getPiece(){
+        return pieceNumber;
     }
     
     //Draws out Ascii art of the gameboard
-    public void draw(){
-        char c;
+    public static void draw(){
         for (int i = 0; i < 8; i++){
-            System.out.println("[");
+            System.out.println(rowOffset + " [");       //Rows starting from 8
             for (int j = 0; j < 8; j++){
                 c = (char)GameBoard[i][j].getAbbrev(pieceNumber);
                 if(isWhite) c=java.lang.Character.toUpperCase(c);
                 System.out.print("," + c + ",");
             }
             System.out.println("]");
-        }    
+            rowOffset--;
+        }   
+        System.out.println("   A  B  C  D  E  F  G  H");     //Letter Grid      
     }    
     
     public char getAbbrev(int pieceNo){
@@ -73,12 +84,56 @@ public class BoardButton {
         
     }
     
+    //White piece constructor
     public BoardButton(int row, int column, int piece){
         this.row=row;
         this.column=column;
         this.pieceNumber=piece;
     }
     
+    //Black piece constructor
+    public BoardButton(int row, int column, int piece, boolean color){
+        this.row=row;
+        this.column=column;
+        this.pieceNumber=piece;
+        this.isPieceWhite=false;
+    }
+    
+    //Testable Main, mostly to check println offsets.
+    public static void Main(String[] args){
+       initializeGame();
+       draw();  
+    }
+    
+    public static void initializeGame(){                          //Black back line
+        GameBoard[0][0] = new BoardButton(0,0,4, false);
+        GameBoard[0][1] = new BoardButton(0,1,2, false);
+        GameBoard[0][2] = new BoardButton(0,2,3, false);
+        GameBoard[0][3] = new BoardButton(0,3,5, false);
+        GameBoard[0][4] = new BoardButton(0,4,6, false);
+        GameBoard[0][5] = new BoardButton(0,5,3, false);
+        GameBoard[0][6] = new BoardButton(0,6,2, false);
+        GameBoard[0][7] = new BoardButton(0,7,4, false);
+        for (int i =0; i<8; i++){                          //Black pawn row
+            GameBoard[1][i] = new BoardButton(1,i,1,false);
+        }
+        for (int i =2; i<6; i++){                           //Empty No-Man's Land
+            for (int j = 0; j<8; j++){
+                GameBoard[i][j] = new BoardButton(i, j, 0);
+            }
+        }      
+        for (int i=0; i<8; i++){                            //White pawn row
+            GameBoard[6][i] = new BoardButton(6, i, 1);
+        }                                                   //White back line
+        GameBoard[7][0] = new BoardButton(0,0,4);
+        GameBoard[7][1] = new BoardButton(0,1,2);
+        GameBoard[7][2] = new BoardButton(0,2,3);
+        GameBoard[7][3] = new BoardButton(0,3,5);
+        GameBoard[7][4] = new BoardButton(0,4,6);
+        GameBoard[7][5] = new BoardButton(0,5,3);
+        GameBoard[7][6] = new BoardButton(0,6,2);
+        GameBoard[7][7] = new BoardButton(0,7,4);
+    }
 }
 
 
