f3ccb2447a17738bf59b7a44844483b52e707e3c
Henry rheault <henry.rheault@gmail.,com>
2019-10-07 18:56:55 -0500

    Added Abbreviation field to BoardButton with a setter verifying format. Potentially Abstract the check code to this method alone or a different class.

--- gradleproject1/src/main/java/gradleproject1/BoardButton.java
+++ gradleproject1/src/main/java/gradleproject1/BoardButton.java
@@ -6,6 +6,7 @@ public class BoardButton {
     int row;
     int column;
     private Piece piece;
+    private String abbreviation;
     /*
     private static final int PAWN=1;
     private static final int KNIGHT=2;
@@ -36,104 +37,17 @@ public class BoardButton {
         this.piece = p;
     }
     
-    
-    
-    public void clicked(){
-        
-    }
-    
-    public BoardButton(int row, int column){
-        this.row=row;
-        this.column=column;
-       
-    }
-    
-     //Grid offset arrays. White at bottom. For black pieces flips position and reads from that value of array
-    private static double[][] gridOffsetPawn = new double[][]
-    {
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {5, 5, 5, 5, 5, 5, 5, 5},
-        {1, 1, 2, 3, 3, 2, 1, 1},
-        {0.5, 0.5, 1, 2.5, 2.5, 1, 0.5, 0.5},
-        {0, 0, 0, 2, 2, 0, 0, 0},
-        {0.5, -0.5, -1, 0, 0, -1, -0.5, 0.5},
-        {0.5, 1, 1, -2, -2, 1, 1, 0.5},
-        {0, 0, 0, 0, 0, 0, 0, 0}
-    };
-    
-    private static double[][] gridOffsetKnight = new double[][]
-    {
-        {-5, -4, -3, -3, -3, -3, -4, -5},
-        {-4, -2, 0, 0, 0, 0, -2, -4},
-        {-3, 0, 1, 1.5, 1.5, 1, 0, -3},
-        {-3, 0.5, 1.5, 2, 2, 1.5, 0.5, -3},
-        {-3, 0.5, 1.5, 2, 2, 1.5, 0.5, -3},
-        {-3, 0.5, 1, 1.5, 1.5, 1, 0.5, -3},
-        {-4, -2, 0, 0.5, 0.5, 0, -2, -4},
-        {-5, -4, -3, -3, -3, -3, -4, -5}
-        
-    };
-    
-    private static double[][] gridOffsetBishop = new double[][]
-    {
-        {-2, -1, -1, -1, -1, -1, -1, -2},
-        {-1, 0, 0, 0, 0, 0, 0, -1},
-        {-1, 0, 0.5, 1, 1, 0.5, 0, -1},
-        {-1, 0.5, 0.5, 1, 1, 0.5, 0.5, -1},
-        {-1, 0, 1, 1, 1, 1, 0, -1},
-        {-1, 1, 1, 1, 1, 1, 1, -1},
-        {-1, 0.5, 0, 0, 0, 0, 0.5, -1},
-        {-2, -1, -1, -1, -1, -1, -1, -2}
-    };
-            
-    private static double[][] gridOffsetRook = new double[][]{
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0.5, 1, 1, 1, 1, 1, 1, 0.5},
-        {-.5, 0, 0, 0, 0, 0, 0, -.5},
-        {-.5, 0, 0, 0, 0, 0, 0, -.5},
-        {-.5, 0, 0, 0, 0, 0, 0, -.5},
-        {-.5, 0, 0, 0, 0, 0, 0, -.5},
-        {-.5, 0, 0, 0, 0, 0, 0, -.5},  
-        {0, 0, 0, 0.5, 0.5, 0, 0, 0}       
-    };
-    
-    private static double[][] gridOffsetQueen = new double[][]{
-        {-2, -1, -1, -0.5, -0.5, -1, -1, -2},
-        {-1, 0, 0, 0, 0, 0, 0, -1},
-        {-1, 0, 0.5, 0.5, 0.5, 0.5, 0, -1},
-        {-0.5, 0, 0.5, 0.5, 0.5, 0.5, 0, -0.5},
-        {0, 0, 0.5, 0.5, 0.5, 0.5, 0, -0.5},
-        {-1, 0.5, 0.5, 0.5, 0.5, 0.5, 0, -1},
-        {-1, 0, 0.5, 0, 0, 0, 0, -1},
-        {-2, -1, -1, -0.5, -0.5, -1, -1, -2}   
-    };
-    
-    private static double[][] gridOffsetKing = new double[][]{
-        {-3, -4, -4, -5, -5, -4, -4, -3},
-        {-3, -4, -4, -5, -5, -4, -4, -3},
-        {-3, -4, -4, -5, -5, -4, -4, -3},
-        {-3, -4, -4, -5, -5, -4, -4, -3},
-        {-2, -3, -3, -4, -4, -3, -3, -2},
-        {-1, -2, -2, -2, -2, -2, -2, -1},
-        {2, 2, 0, 0, 0, 0, 2, 2},
-        {2, 3, 1, 0, 0, 1, 3, 2} 
-    };
-    
-    
-    /** @Author Henry Rheault
+    /**
+     *@author Henry Rheault 
      * 
-     * Method to access grid offset points for AI points calculation.
-     * @param location string, 2 characters, IE: E4, not case sensitive
-     * @param piece, single char, not case sensitive
-     * @param team, white true black false.
-     * 
-     * @Return double (wrapper) for offset to add to points value for AI calculation.
-    */
-    public Double getGridOffset(String location, char piece, boolean team){
-        boolean userErrorFlag = false;                      //User F'd up or not?
-        try {
-            assert (location.length() == 2);
-            char[] temp = location.toCharArray();
+     * Method for assigning the boardbutton a chessboard abbreviation that it can
+     * simply report back. Verifies format with code cloned from GridOffset
+     */
+    public void setAbbreviation(String s){
+        boolean userErrorFlag = false;
+        try{
+            assert (s.length() == 2);
+            char[] temp = s.toCharArray();
 
             //Take string, convert to chars, and get Memory Array location from the Chess String location
             char col = temp[0];                             //A-H, not case sensitive, input argument
@@ -147,62 +61,34 @@ public class BoardButton {
                 userErrorFlag = true;                        //Make sure row given is less than 8
                 assert (r0w <=8 && r0w>0);                   //Set user error flag and break out of try if not
             }                     
+            
             //Format verified, convert Letter-Number to Array indexes
-            short column = (short) (col - 'A');              //A is 0, B is 1, etc. Returns numeric difference between A & input letter
-            short row2 = (short) r0w;                        //cast Char to Short because it gets mad if you cast to int
-            row = row2 + 8;                                  //Add 8 to convert chess layout to Memory layout
+            this.abbreviation = s;
             
-            //Flip the black team's characters back to lowercase
-            if (team) {
-                piece = Character.toUpperCase(piece);       //Does nothing if true but kept for readability
-            } else {
-                piece = Character.toLowerCase(piece);
-            }
-            
-            switch (piece) {
-                //white pieces
-                case 'P':
-                    return gridOffsetPawn[row][column];
-                case 'N':
-                    return gridOffsetKnight[row][column];
-                case 'B':
-                    return gridOffsetBishop[row][column];
-                case 'R':
-                    return gridOffsetRook[row][column];
-                case 'Q':
-                    return gridOffsetQueen[row][column];
-                case 'K':
-                    return gridOffsetKing[row][column];
-                //here be black pieces    
-                case 'p':
-                    return gridOffsetPawn[8 - row][8 - column];
-                case 'n':
-                    return gridOffsetKnight[8 - row][8 - column];
-                case 'b':
-                    return gridOffsetBishop[8 - row][8 - column];
-                case 'r':
-                    return gridOffsetRook[8 - row][8 - column];
-                case 'q':
-                    return gridOffsetQueen[8 - row][8 - column];
-                case 'k':
-                    return gridOffsetKing[8 - row][8 - column];
-                //Should never get here but best not to break things with a stupid value    
-                default:
-                    return (double)0;
-            }
-        } catch (Exception e) {
-            if(location.length()>2|| userErrorFlag) {
-                System.out.println("Error, invalid move. Retrying:");
-                return null;
-            }
-            
+        } catch (Exception e){
+            if (s.length()!=2 || userErrorFlag){
+                System.out.println("You did something wrong. Invalid format.");
+            }    
             else {
-                System.out.println("Offset program fucked up! Returning default of 0 offset.");
-                return (double)0;
-            }
-        }
+                System.out.println("Some shit went down.");
+                e.printStackTrace();
+            }    
+        }    
+    }    
+    
+    public String getAbbreviation(){
+        return this.abbreviation;
     }
-  
+    
+    public void clicked(){
+        
+    }
+    
+    public BoardButton(int row, int column){
+        this.row=row;
+        this.column=column;
+       
+    }
     
     //Testable Main, mostly to check println offsets.
     public static void Main(String[] args){            
