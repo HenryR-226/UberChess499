3e2877d775cf78433b1d6831d976c3fb68dcd04f
Oleg Semenenko <olegsemenenko@yahoo.com>
2019-12-11 01:43:17 -0600

    Small changes

--- gradleproject1/src/main/java/gradleproject1/Move.java
+++ gradleproject1/src/main/java/gradleproject1/Move.java
@@ -14,7 +14,7 @@ public class Move {
 	private int oldrow, oldcol;
 	private BoardButton n3w;
 	private int newrow, newcol;
-	
+
 	private String abbreviation;
 	private Piece piece;
 	private Piece captured = null;
@@ -25,9 +25,9 @@ public class Move {
 	private static Player blackPlayer;
 
 	private static Board b;
-	
+
 	private static int movesWithoutCapture = 0;
-	
+
 	/**
 	 * @author Henry Rheault
 	 *
@@ -36,8 +36,8 @@ public class Move {
 	 *         the error is within Move or elsewhere.
 	 * 
 	 *         Adds a move to a particular gamer's move list for demo purposes.
-	 *         
-	 *         @deprecated
+	 * 
+	 * @deprecated
 	 */
 	public Move(Piece p, char row, char column) throws Exception {
 		try {
@@ -74,16 +74,20 @@ public class Move {
 				blackPlayer.addMove(this);
 
 			String location = this.n3w.getAbbreviation();
-			p.setLocation(location); 
+			p.setLocation(location);
 			this.getPiece().incRank();
 			this.getPiece().madeFirstMove();
-			
+			p.col = n;
+			p.row = m;
+
 		} catch (Exception e) {
 			System.out.println("Invalid move constructor. Try again.");
 		}
 	}
-	
-	public Move(Piece p, BoardButton button, boolean thisIsAMoveGeneratorOnlyWillNotPushAMove) throws Exception { // Overloaded																											// boardbutton
+
+	public Move(Piece p, BoardButton button, boolean thisIsAMoveGeneratorOnlyWillNotPushAMove) throws Exception { // Overloaded
+																													// //
+																													// boardbutton
 		// to attempt a move to
 		try {
 			BoardButton[][] GameBoard = b.getGameBoard(); // Fetch gameboard object
@@ -93,20 +97,23 @@ public class Move {
 			int j = (int) loc[1] - '0' - 1;
 
 			this.old = GameBoard[i][j];
-			//System.out.println("Calling remove on boardbutton" + i + " " + j);
+			// System.out.println("Calling remove on boardbutton" + i + " " + j);
 			this.n3w = button;
 			this.piece = p;
 			String abbrev = n3w.getAbbreviation();
-			//System.out.println("Line 92 in Move, " + p.getAbbrev() + " moved from " + loc[0] + loc[1] + " to " + abbrev + ".");
+			// System.out.println("Line 92 in Move, " + p.getAbbrev() + " moved from " +
+			// loc[0] + loc[1] + " to " + abbrev + ".");
 
 			String move = String.valueOf(p.getAbbrev());
 			if (n3w.getPiece() != null) {
 				move = move + "x"; // x means a piece captured the piece on it's destination square
 				capture = true;
 				this.captured = n3w.getPiece();
-			} 
+			}
 			move = move + abbrev;
 			setAbbreviation(move);
+			p.col = i;
+			p.row = j;
 
 		} catch (Exception e) {
 			System.out.println("Invalid move constructor taking board square. Try again.");
@@ -114,7 +121,6 @@ public class Move {
 		}
 	}
 
-	
 	public Move(Piece p, BoardButton button) throws Exception { // Overloaded constructor, simply declare a boardbutton
 																// to attempt a move to
 		try {
@@ -126,14 +132,14 @@ public class Move {
 			int j = (int) loc[1] - '0' - 1;
 
 			this.old = GameBoard[i][j];
-			//System.out.println("Calling remove on boardbutton" + i + " " + j);
+			// System.out.println("Calling remove on boardbutton" + i + " " + j);
 			this.n3w = button;
 			this.piece = p;
 			String abbrev = n3w.getAbbreviation();
 			System.out.println(p.getAbbrev() + " moved from " + loc[0] + loc[1] + " to " + abbrev + ".");
 
 			String move = String.valueOf(p.getAbbrev());
-			if (n3w.getPiece()!=null) {
+			if (n3w.getPiece() != null) {
 				move = move + "x"; // x means a piece captured the piece on it's destination square
 				capture = true;
 				this.captured = n3w.getPiece();
@@ -142,33 +148,36 @@ public class Move {
 			move = move + abbrev;
 			setAbbreviation(move);
 
-			if (p.isWhite()) {				
-				//Check move implementations, needs to be tested. 11/29/2019				
-				if (whitePlayer.inCheck()) 		//If we're in check, make sure this takes us out of check
-					if (!whitePlayer.getKing().isInCheck(GameBoard, whitePlayer.getPieceList(), this.piece, this)) whitePlayer.removeCheck();
-					//else return null because you can't push this move
+			if (p.isWhite()) {
+				// Check move implementations, needs to be tested. 11/29/2019
+				if (whitePlayer.inCheck()) // If we're in check, make sure this takes us out of check
+					if (!whitePlayer.getKing().isInCheck(GameBoard, whitePlayer.getPieceList(), this.piece, this))
+						whitePlayer.removeCheck();
+				// else return null because you can't push this move
 				whitePlayer.addMove(this);
 			} else {
-				if (blackPlayer.inCheck()) 
-					if (!blackPlayer.getKing().isInCheck(GameBoard, whitePlayer.getPieceList(), this.piece, this)) blackPlayer.removeCheck();
-					//else return null because you can't push this move
+				if (blackPlayer.inCheck())
+					if (!blackPlayer.getKing().isInCheck(GameBoard, whitePlayer.getPieceList(), this.piece, this))
+						blackPlayer.removeCheck();
+				// else return null because you can't push this move
 				blackPlayer.addMove(this);
 			}
 			String location = this.n3w.getAbbreviation();
-			//Update pawn first move field
-			if (this.piece.getAbbrev() == 'p' || this.piece.getAbbrev()=='P') {
+			// Update pawn first move field
+			if (this.piece.getAbbrev() == 'p' || this.piece.getAbbrev() == 'P') {
 				if (this.piece.firstMove()) {
 					piece.madeFirstMove();
-					
-					if (Math.abs(this.n3w.getColumn() - this.old.getColumn()) == 2) this.getPiece().incRank();
+
+					if (Math.abs(this.n3w.getColumn() - this.old.getColumn()) == 2)
+						this.getPiece().incRank();
 				}
-				if (this.piece.getRank()==5 && ((Pawn) this.piece).canPromote()) { 
+				if (this.piece.getRank() == 5 && ((Pawn) this.piece).canPromote()) {
 					try {
-						((Pawn)this.piece).promote();
+						((Pawn) this.piece).promote();
 					} catch (Exception e) {
-						
+
 					}
-				
+
 				}
 				this.getPiece().incRank();
 				this.getPiece().madeFirstMove();
@@ -176,10 +185,15 @@ public class Move {
 			old.removePiece();
 			n3w.setPiece(p);
 			p.setLocation(location);
-			if (p.getAbbrev()=='p' || p.getAbbrev()=='P') movesWithoutCapture = 0;
-			
-			//Stalemate!!
-			if (movesWithoutCapture == 50 || (b.getBlackPlayer().getPieceList().size()==1 && b.getWhitePlayer().getPieceList().size()==1)) {
+			p.col = i;
+			p.row = j;
+
+			if (p.getAbbrev() == 'p' || p.getAbbrev() == 'P')
+				movesWithoutCapture = 0;
+
+			// Stalemate!!
+			if (movesWithoutCapture == 50 || (b.getBlackPlayer().getPieceList().size() == 1
+					&& b.getWhitePlayer().getPieceList().size() == 1)) {
 				System.out.println("50 move or King-only stalemate! Game's over. Line 171 Move");
 				Player.getGameState().setWinrar(null);
 				Player.getGameState().setStalemate();
@@ -190,10 +204,11 @@ public class Move {
 			e.printStackTrace();
 		}
 	}
+
 	/**
 	 * Move constructor for pawn promotion only, straight up column
 	 */
-	public Move (Pawn p) {
+	public Move(Pawn p) {
 		assert (p.getRow() == 6 || p.getRow() == 1);
 		BoardButton[][] GameBoard = b.getGameBoard(); // Fetch gameboard object
 
@@ -203,28 +218,32 @@ public class Move {
 		int j = (int) loc[1] - '0' - 1;
 
 		this.old = GameBoard[i][j];
-		//System.out.println("Calling remove on boardbutton" + i + " " + j);
-		if (p.isWhite) this.n3w = GameBoard[p.getCol()][7];
-		if (!p.isWhite) this.n3w = GameBoard[p.getCol()][0];
-		
-		//this.piece = p;
+		// System.out.println("Calling remove on boardbutton" + i + " " + j);
+		if (p.isWhite)
+			this.n3w = GameBoard[p.getCol()][7];
+		if (!p.isWhite)
+			this.n3w = GameBoard[p.getCol()][0];
+
+		// this.piece = p;
 		String abbrev = n3w.getAbbreviation();
 		String move = String.valueOf(p.getAbbrev());
 		movesWithoutCapture = 0;
 		move = move + abbrev;
 		setAbbreviation(move);
-		
-		Piece promoted = p.promote(); 
-		
+
+		Piece promoted = p.promote();
+		p.col = i;
+		p.row = j;
+
 		old.removePiece();
 		n3w.setPiece(promoted);
-		
+
 		this.piece = promoted;
 		promoted.setLocation(abbrev);
 	}
-	
-	//Overloaded, boolean true = steps RIGHT, false = steps LEFT
-	public Move (Pawn p, boolean side) {
+
+	// Overloaded, boolean true = steps RIGHT, false = steps LEFT
+	public Move(Pawn p, boolean side) {
 		assert (p.getRow() == 6 || p.getRow() == 1);
 		BoardButton[][] GameBoard = b.getGameBoard(); // Fetch gameboard object
 
@@ -234,39 +253,44 @@ public class Move {
 		int j = (int) loc[1] - '0' - 1;
 
 		this.old = GameBoard[i][j];
-		//System.out.println("Calling remove on boardbutton" + i + " " + j);
+		// System.out.println("Calling remove on boardbutton" + i + " " + j);
 		if (p.isWhite) {
-			if (side) this.n3w = GameBoard[p.getCol()+1][7];
-			else this.n3w = GameBoard[p.getCol()-1][7];
+			if (side)
+				this.n3w = GameBoard[p.getCol() + 1][7];
+			else
+				this.n3w = GameBoard[p.getCol() - 1][7];
 		}
 		if (!p.isWhite) {
-			if (side) this.n3w = GameBoard[p.getCol()+1][0];
-			else this.n3w = GameBoard[p.getCol()-1][0];
+			if (side)
+				this.n3w = GameBoard[p.getCol() + 1][0];
+			else
+				this.n3w = GameBoard[p.getCol() - 1][0];
 		}
-		
-		//this.piece = p;
+
+		// this.piece = p;
 		String abbrev = n3w.getAbbreviation();
 		String move = String.valueOf(p.getAbbrev());
 		String x = "x";
 		movesWithoutCapture = 0;
 		move = move + x + abbrev;
-		if (n3w.isFull()) captured = n3w.getPiece();
+		if (n3w.isFull())
+			captured = n3w.getPiece();
 		setAbbreviation(move);
-		
-		Piece promoted = p.promote(); 
-		
+
+		Piece promoted = p.promote();
+
 		old.removePiece();
 		n3w.removePiece(promoted);
 		n3w.setPiece(promoted);
-		
+
 		this.piece = promoted;
 		promoted.setLocation(abbrev);
 	}
-	
-	/** @author Henry Rheault
-	 * Generates and returns a move based on a input string. 
-	 * Passed in 
-	 * @throws Exception 
+
+	/**
+	 * @author Henry Rheault Generates and returns a move based on a input string.
+	 *         Passed in
+	 * @throws Exception
 	 */
 	public Move newMove(Piece p, BoardButton button) throws Exception {
 		Move m = new Move(p, button);
@@ -306,15 +330,15 @@ public class Move {
 	public static void setBlackPlayer(Player black) {
 		blackPlayer = black;
 	}
-	
+
 	public Piece getPiece() {
 		return this.piece;
 	}
-	
+
 	public Piece getCaptured() {
 		return this.captured;
 	}
-	
+
 	public boolean wasCaptured() {
 		return capture;
 	}
--- gradleproject1/src/main/java/gradleproject1/Pawn.java
+++ gradleproject1/src/main/java/gradleproject1/Pawn.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Pawn extends Piece {
-	// int pawnID; //ID of the pawn
-	//boolean firstMove = true; 	// If it is the pawn's first move allowing a double jump
-	boolean canAttack; 			// If the pawn can attack
-	boolean blocked; 			// currently blocked in front?
-	char column; 				// Pawn's "home" column
+
+	boolean canAttack; // If the pawn can attack
+	boolean blocked; // currently blocked in front?
+	char column; // Pawn's "home" column
 	int col, row;
 	Board b;
 	private static Scanner scan;
 
-	public Pawn(String pawnID, boolean team, Board b, int row, int col) {
+	public Pawn(String pawnID, boolean team, Board b, int col, int row) {
 		this.setRow(row);
 		this.setCol(col);
 		String loc = Character.toString((char) ((char) row + 'A'));
 		loc = loc + Integer.toString(col + 1);
-		//System.out.println("Pawn internal loc set as " + loc + ", line 17 in Pawn"); 	//- tested and works, 12/1/2019, certified not a problem
+		// System.out.println("Pawn internal loc set as " + loc + ", line 17 in Pawn");
+		// //- tested and works, 12/1/2019, certified not a problem
 		this.setLocation(loc);
 		this.setName("Pawn");
 		this.pieceID = pawnID;
@@ -26,8 +26,7 @@ public class Pawn extends Piece {
 		if (team) {
 			setAbbreviation('P');
 			this.player = b.getWhitePlayer();
-		}
-		else if (!team) {
+		} else if (!team) {
 			setAbbreviation('p');
 			this.player = b.getBlackPlayer();
 		}
@@ -45,47 +44,49 @@ public class Pawn extends Piece {
 
 		ArrayList<Integer> cords = BoardButton.toArray(location);
 		int x = cords.get(0);
-		//System.out.println("X set to " + x + " , line 39 pawn");
-		//FIXME - This is probably not right, this references line 56 of BoardButton. ONLY pawn was having an off by one issue. 12/1/2019
-		int y = cords.get(1)-1;			//Problem- crashes program when accessed on Black players- line 114 below
-		//System.out.println("Y ste to " + y + " , line 42 pawn");
+		// System.out.println("X set to " + x + " , line 39 pawn");
+		// FIXME - This is probably not right, this references line 56 of BoardButton.
+		// ONLY pawn was having an off by one issue. 12/1/2019
+		int y = cords.get(1) - 1; // Problem- crashes program when accessed on Black players- line 114 below
+		// System.out.println("Y ste to " + y + " , line 42 pawn");
 		boolean team = p.isWhite();
 
 		BoardButton highSide;
 		BoardButton lowSide;
 		BoardButton front;
 
-		//System.out.println("\nLocation Entrered: " + location + "\n");
+		// System.out.println("\nLocation Entrered: " + location + "\n");
 
-		//System.out.println("Moves Found:");
+		// System.out.println("Moves Found:");
 		if (team) { // White pawn, goes up
 
 			if (x + 1 < 8 && y + 1 < 9) {
 
 				highSide = board[x + 1][y + 1];
-				//System.out.println("HighSide set to: " + (x + 1) + " " + (y + 1));
+				// System.out.println("HighSide set to: " + (x + 1) + " " + (y + 1));
 			} else {
 				highSide = null;
 				// System.out.println("Highside set to: null ");
 			}
 			if (x - 1 > -1 && y + 1 < 9) {
 				lowSide = board[x - 1][y + 1];
-				//System.out.println("LowSide set to: " + (x - 1) + " " + (y + 1));
+				// System.out.println("LowSide set to: " + (x - 1) + " " + (y + 1));
 			} else {
 				lowSide = null;
-				//System.out.println("Lowside set to: null ");
+				// System.out.println("Lowside set to: null ");
 			}
 			if (y + 1 < 9) {
 				front = board[x][y + 1];
-				//System.out.println("Front set to: " + x + " " + (y + 1));
+				// System.out.println("Front set to: " + x + " " + (y + 1));
 			} else {
 				front = null;
-				//System.out.println("Front set to: null");
+				// System.out.println("Front set to: null");
 			}
 			if (p.firstMove()) {
 				BoardButton front2 = board[x][y + 2];
 				if (front != null && !front2.isFull() && !front.isFull()) {
-					//System.out.println("Pawn's first move! Should add " + front2.getAbbreviation() + " to list!");
+					// System.out.println("Pawn's first move! Should add " +
+					// front2.getAbbreviation() + " to list!");
 
 					result.add(front2);
 				}
@@ -100,26 +101,26 @@ public class Pawn extends Piece {
 
 		} else { // Black team, pawn down
 			if (x + 1 < 8 && y - 1 > -1) {
-				//System.out.println("X value "  + x + " , Y Value " + y);
+				// System.out.println("X value " + x + " , Y Value " + y);
 				highSide = board[x + 1][y - 1];
-				//System.out.println("HighSide set to: " + (x + 1) + " " + (y - 1));
+				// System.out.println("HighSide set to: " + (x + 1) + " " + (y - 1));
 			} else {
 				highSide = null;
-				//System.out.println("Highside set to null ");
+				// System.out.println("Highside set to null ");
 			}
 			if (x - 1 > -1 && y - 1 > -1) {
 				lowSide = board[x - 1][y - 1];
-				//System.out.println("LowSide set to: " + (x - 1) + " " + (y - 1));
+				// System.out.println("LowSide set to: " + (x - 1) + " " + (y - 1));
 			} else {
 				lowSide = null;
-				//System.out.println("Lowside set to null ");
+				// System.out.println("Lowside set to null ");
 			}
 			if (y - 1 > -1) {
 				front = board[x][y - 1];
-				//System.out.println("Front set to: " + x + " " + (y - 1));
+				// System.out.println("Front set to: " + x + " " + (y - 1));
 			} else {
 				front = null;
-				//System.out.println("Front set to null");
+				// System.out.println("Front set to null");
 			}
 			if (p.firstMove()) {
 				BoardButton front2 = board[x][y - 2];
@@ -137,48 +138,51 @@ public class Pawn extends Piece {
 		}
 		return result;
 	}
-	
+
 	public boolean canPromote() {
 		if (this.rank == 5) {
-			//BoardButton b;
+			// BoardButton b;
 			if (this.isWhite) {
-				if (!bb[this.col][++this.row].isFull()) return true;
-				else return false;
-			}
-			else {
-				if (!bb[this.col][--this.row].isFull()) return true;
-				else return false;
+				if (!bb[this.col][++this.row].isFull())
+					return true;
+				else
+					return false;
+			} else {
+				if (!bb[this.col][--this.row].isFull())
+					return true;
+				else
+					return false;
 			}
 		}
 		return false;
 	}
-	
+
 	/**
-	 * @author Henry Rheault
-	 * Helper Method for do-while in promote() method, returns false if invalid or pulls char out of the s.nextLine
-	 * returns false if pawn or king entered, returns null as default
+	 * @author Henry Rheault Helper Method for do-while in promote() method, returns
+	 *         false if invalid or pulls char out of the s.nextLine returns false if
+	 *         pawn or king entered, returns null as default
 	 */
-	
+
 	private boolean isValid(Character c) {
 		switch (Character.toUpperCase(c)) {
-			case 'Q':
-				return true;
-			case 'N':
-				return true;
-			case 'R':	
-				return true;
-			case 'B':
-				return true;
-			default:	
-				return false;
+		case 'Q':
+			return true;
+		case 'N':
+			return true;
+		case 'R':
+			return true;
+		case 'B':
+			return true;
+		default:
+			return false;
 		}
 	}
-	
+
 	/**
-	 * @author Henry Rheault
-	 * Handles promotions, if rank == 6 (after it moves to the end of the board) will handle dialog of what piece to construct in it's place 
-	 * (Realistically only Queen, or Knight in the case of a super big brain gamer)
-	 * Returns piece created to be added to the board
+	 * @author Henry Rheault Handles promotions, if rank == 6 (after it moves to the
+	 *         end of the board) will handle dialog of what piece to construct in
+	 *         it's place (Realistically only Queen, or Knight in the case of a
+	 *         super big brain gamer) Returns piece created to be added to the board
 	 */
 	public Piece promote() {
 		Piece result = null;
@@ -186,65 +190,17 @@ public class Pawn extends Piece {
 			String s;
 			Character c = 'Z';
 			System.out.println("Pawn reached end of board, type character of piece you'd like to promote it to:");
-			do {
-				c = scan.nextLine().charAt(0);
-				if (isValid(c))
-					break;
-				else
-					System.out.println("Invalid character. Try again:");
-			} while (scan.hasNextLine() && !isValid(c));
-			switch (c) { // Not toUpperCase, we're going to use the case to determine the team and deal
-							// with it here
-			case 'Q':
-				this.player.removePiece(this);
-				Queen promotedQueen = new Queen("promotedQueen", true, this.player.getBoard(), this.row, this.col);
-				this.player.addPiece(promotedQueen);
-				result = promotedQueen;
-				break;
-			case 'q':
-				this.player.removePiece(this);
-				Queen promotedqueen = new Queen("promotedQueen", false, this.player.getBoard(), this.row, this.col);
-				this.player.addPiece(promotedqueen);
-				result = promotedqueen;
-				break;
-			case 'N':
-				this.player.removePiece(this);
-				Knight promotedKnight = new Knight("promotedKnight", true, this.player.getBoard(), this.row, this.col);
-				this.player.addPiece(promotedKnight);
-				result = promotedKnight;
-				break;
-			case 'n':
-				this.player.removePiece(this);
-				Knight promotedknight = new Knight("promotedKnight", false, this.player.getBoard(), this.row, this.col);
-				this.player.addPiece(promotedknight);
-				result = promotedknight;
-				break;
-			case 'B':
-				this.player.removePiece(this);
-				Bishop promotedBishop = new Bishop("promotedBishop", true, this.player.getBoard(), this.row, this.col);
-				this.player.addPiece(promotedBishop);
-				result = promotedBishop;
-				break;
-			case 'b':
-				this.player.removePiece(this);
-				Bishop promotedbishop = new Bishop("promotedBishop", false, this.player.getBoard(), this.row, this.col);
-				this.player.addPiece(promotedbishop);
-				result = promotedbishop;
-				break;
-			case 'R':
-				this.player.removePiece(this);
-				Rook promotedRook = new Rook("promotedRook", true, this.player.getBoard(), this.row, this.col);
-				this.player.addPiece(promotedRook);
-				result = promotedRook;
-				break;
-			case 'r':
-				this.player.removePiece(this);
-				Rook promotedrook = new Rook("promotedrook", false, this.player.getBoard(), this.row, this.col);
-				this.player.addPiece(promotedrook);
-				result = promotedrook;
-				break;
-			}	//Do NOT close the scan, it's the main's scan as well!!!
-		} 
+			this.player.removePiece(this);
+			Queen promotedQueen = new Queen("promotedQueen", true, this.player.getBoard(), this.row, this.col);
+			this.player.addPiece(promotedQueen);
+			result = promotedQueen;
+			BoardButton[][] GameBoard = b.getGameBoard();
+			BoardButton a;
+			a = GameBoard[row][col];
+			a.setPiece(promotedQueen);
+			b.draw(b);
+
+		}
 		return result;
 	}
 
